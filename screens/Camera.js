7b2c3807922c7f732ca3f4bdd2481e30659bc63b
ImJPeti <jovanovicsp@gmail.com>
2023-02-25 20:37:15 +0100

    Text recognize

---
+++ screens/Camera.js
@@ -0,0 +1,36 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import Constants from "expo-constants";
+import callGoogleVisionAsync from "../helperFunction";
+import ImagePickerComponent from "./ImagePickerComponent";
+
+export default function Camera() {
+  const [imagePath, setImagePath] = useState("");
+  const [text, setText] = useState("");
+
+  const handleChange = (event) => {
+    setImage(URL.createObjectURL(event.target.files[0]));
+  };
+
+  return (
+    <View style={styles.container}>
+      <ImagePickerComponent onSubmit={callGoogleVisionAsync} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Camera from './Camera'
 
 const HomeScreen = () => {
   return (
     <View>
-      <Text>Home</Text>
+     <Camera />
     </View>
   )
 }
