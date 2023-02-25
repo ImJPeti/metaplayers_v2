128ca498ec1f7ad9aef97fc146f1fff1e637da18
ImJPeti <jovanovicsp@gmail.com>
2023-02-25 20:48:37 +0100

    Done with implementation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from "react-native";
 import React from 'react'
 import Camera from './Camera'
+import NavBar from './navBar'
 
 const HomeScreen = () => {
   return (
-    <View>
-     <Camera />
+    <View style={styles.container}>
+      <NavBar />
     </View>
-  )
+  );
 }
-
 export default HomeScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#262338",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative",
+  },
+});
